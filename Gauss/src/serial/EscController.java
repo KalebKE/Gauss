80c39248b7d528e880dda01a08be77e26942f560
KalebKE <kaleb@kircherelectronics.com>
2014-12-12 00:52:46 +0000

    The steering servo and electronic speed controller are confirmed to be working.

--- Gauss/src/serial/EscController.java
+++ Gauss/src/serial/EscController.java
@@ -4,8 +4,8 @@ public class EscController
 {
 	private final int STANDARD_SERVO_CHANNEL = 0;
 
-	private final int escMin = 100;
-	private final int escMax = 200;
+	private final int escMin = 122;
+	private final int escMax = 615;
 
 	private final int escRange = escMax - escMin;
 
@@ -13,7 +13,7 @@ public class EscController
 
 	private final int speedMax = 100;
 
-	private final double PerPulse = (double) escRange / (double) speedMax;
+	private final double perPulse = (double) escRange / (double) speedMax;
 
 	private AdafruitPCA9685 servoBoard;
 
@@ -24,9 +24,18 @@ public class EscController
 		testEsc();
 	}
 
+	/**
+	 * Forward = 50-100. Backwards = 0-50. Netural is 50.
+	 * 
+	 * Note that the brake is apparently 0ms and 252ms, but I haven't found the
+	 * full range of the ESC yet, so I am unsure what they are in terms of an
+	 * int for speed. Brake is also hard on the engine.
+	 * 
+	 * @param speed
+	 */
 	public void setSpeed(int speed)
 	{
-		int pulse = (int) ((PerPulse * speed) + escMin);
+		int pulse = (int) ((perPulse * speed) + escMin);
 
 		if (pulse <= escMax)
 		{
@@ -41,15 +50,17 @@ public class EscController
 		// The Traxxus servo uses a ency of 50Hz and a 180 degree rotation
 		// range with a 100 centisecond to 200 centisecond pulse range.
 		servoBoard.setPWMFreq(60);
-		
+
 		// ESC must be plugged in within two seconds of starting the code.
 		// The ESC is looking of the neutral position, so we send it that...
-		servoBoard.setPWM(STANDARD_SERVO_CHANNEL, 0, 221);
-		servoBoard.waitfor(10000);
+		setSpeed(50);
+		servoBoard.waitfor(2000);
 	}
 
 	private void testEsc()
 	{
-		servoBoard.setPWM(STANDARD_SERVO_CHANNEL, 0, 400);
+		setSpeed(60);
+		servoBoard.waitfor(5000);
+		setSpeed(50);
 	}
 }
